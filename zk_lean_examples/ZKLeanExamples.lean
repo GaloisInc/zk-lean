328a5bd5766347f2e0184001047412ac92f0a124
James Parker <james@galois.com>
2026-01-20 17:48:17 -0500

    Include Keccak

--- zk_lean_examples/ZKLeanExamples.lean
+++ zk_lean_examples/ZKLeanExamples.lean
@@ -1,5 +1,6 @@
 import ZKLeanExamples.AND
-import ZKLeanExamples.AssertLTE -- This one is slow, ~ 6 minutes
+-- import ZKLeanExamples.AssertLTE -- This one is slow, ~ 6 minutes
 import ZKLeanExamples.XOR
+import ZKLeanExamples.Keccak.Spec
 
 def main : IO Unit := IO.println "Done"
